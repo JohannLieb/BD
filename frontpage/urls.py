5328b00211947f4482513d6cb7b1fa47f0f0b63d
JohannLieb <gerbertwells101@gmail.com>
2018-06-09 19:55:19 +0300

    new pathes

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import (
     products_controller,
     RegisterController,
     LoginController,
     LogoutController,
-    logout
+    ProfileController,
+    OrderController,
+    ProfChangeController,
+    NewOrderController
 )
 
 urlpatterns = [
@@ -12,8 +16,11 @@ urlpatterns = [
     path(r'register', RegisterController.as_view(), name='register'),
     path(r'products', products_controller, name='products'),
     path(r'login', LoginController.as_view(), name='login'),
-    path(r'logout', LogoutController.as_view(), name='login'),
-    path(r'logout', logout, name='login'),
+    path(r'logout', login_required(login_url='/login')(LogoutController.as_view()), name='login'),
+    path(r'profile', login_required(login_url='/login')(ProfileController.as_view()), name='profile'),
+    path(r'orders', login_required(login_url='/login')(OrderController.as_view()), name='orders'),
+    path(r'profchange', login_required(login_url='/login')(ProfChangeController.as_view()), name='profchange'),
+    path(r'orders/new', login_required(login_url='/login')(NewOrderController.as_view()), name='new_order')
 
 ]
 
