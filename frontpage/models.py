dc3bc8ff36d3c5a6ebc0cabc2dbb2fb6e4612235
JohannLieb <gerbertwells101@gmail.com>
2018-06-09 19:47:28 +0300

    update database

--- frontpage/models.py
+++ frontpage/models.py
@@ -5,7 +5,10 @@
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from django.utils import timezone
+
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
 class Characteristics(models.Model):
@@ -25,10 +28,34 @@ class Clients(models.Model):
     telno = models.CharField(max_length=15, blank=True, null=True)
     login = models.CharField(max_length=15, blank=True, null=True)
     userpass = models.CharField(max_length=20, blank=True, null=True)
+    last_login = models.TimeField(default=timezone.now())
 
     class Meta:
         managed = False
         db_table = 'clients'
+
+    @property
+    def humanized_full_name(self):
+        if not self.fullname or len(self.fullname.split(',')) < 2:
+            return 'Unknown Unknown'
+        name, surname = self.fullname.lstrip('(').rstrip(')').split(',')
+        return f'{name} {surname}'
+
+    @staticmethod
+    def cast_to_shitty_type(fullname):
+        name, surname = fullname.split()
+        return f'({",".join((name, surname))})'
+
+    @property
+    def address_full(self):
+        if not self.address or len(self.address.split(',')) < 3:
+            return 'Unknown Unknown'
+        street, city, country = self.address.lstrip('("').rstrip(')"').split('"')
+        return f'{street} {city} {country}'
+
+    @property
+    def is_authenticated(self):
+        return True
 
 
 class Log(models.Model):
@@ -45,20 +72,25 @@ class Log(models.Model):
 class Orders(models.Model):
     orders_id = models.AutoField(primary_key=True)
     clients = models.ForeignKey(Clients, models.DO_NOTHING, blank=True, null=True)
-    date_finish = models.DateField(blank=True, null=True)
-    date_start = models.DateField(blank=True, null=True)
+
     orders_date = models.DateField(blank=True, null=True)
     paid = models.NullBooleanField()
     price = models.TextField(blank=True, null=True)  # This field type is a guess.
     staff = models.ForeignKey('Staff', models.DO_NOTHING, blank=True, null=True)
-    status_work = models.NullBooleanField()
-    text = models.TextField(blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
+    product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
+
+    description = models.TextField(blank=True, null=True)
+
 
 
     class Meta:
-        # managed = False
+        managed = False
         db_table = 'orders'
+
+    @property
+    def money_price(self):
+        return self.price
+
 
 
 class Position(models.Model):
@@ -73,7 +105,7 @@ class Position(models.Model):
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    orders = models.ForeignKey(Orders, models.DO_NOTHING, blank=True, null=True)
+    # orders = models.ForeignKey(Orders, models.DO_NOTHING, blank=True, null=True)
     supplier = models.ForeignKey('Supplier', models.DO_NOTHING, blank=True, null=True)
     type = models.CharField(max_length=15, blank=True, null=True)
 
@@ -109,6 +141,9 @@ class Staff(models.Model):
         managed = False
         db_table = 'staff'
 
+    def save(self, *args, **kwargs):
+        self.userpass = make_password(self.userpass)
+        super().save(*args, **kwargs)
 
 class Supplier(models.Model):
     supplier_id = models.AutoField(primary_key=True)
